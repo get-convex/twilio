8192b87685510117d615ff7f37634dc173290bde
Pasha Bitz <pasha@convex.dev>
2024-09-04 16:26:33 -0700

    Removed example

--- example/convex/convex.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineApp } from "convex/server";
-import component from "../../src/component/convex.config";
-
-const app = defineApp();
-app.use(component, { name: "theComponent" });
-
-export default app;
